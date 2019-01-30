bd6015624853bc8e01b62b5da9499a68a8e03798
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-01-29 16:12:02 -0800

    added Fizz test and passed

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,16 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function fizzBuzz(input) {
+    if(input % 3 === 0) {
+        return 'Fizz';
+    };
+}
+
+test('test if input number is divisible by 3', function(assert) {
+    const inputNumber = 3;
+    const result = fizzBuzz(inputNumber);
+    const expected = 'Fizz';
+
+    assert.equal(result, expected);
 });
+
