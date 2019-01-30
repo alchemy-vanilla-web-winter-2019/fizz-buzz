21f55278f2c7394897f68cd30d78ae6501d54129
Cheri Portman <cheriportman@hotmail.com>
2019-01-29 16:17:46 -0800

    Fizz test for 3 created and passed

--- test/tests.js
+++ test/tests.js
@@ -3,13 +3,23 @@ const test = QUnit.test;
 function fizzy(inputNumber) {
     if(inputNumber % 3 === 0 && inputNumber % 5 === 0) {
         return 'FizzBuzz';
+    } else if(inputNumber % 3 === 0) {
+        return 'Fizz';
     }
+
 }
 
 
-test('time to write a test', function(assert) {
+test('FizzBuzz test - number divisible by 3 and 5', function(assert) {
     const inputNumber = 15;
     const expected = 'FizzBuzz';
+    const result = fizzy(inputNumber);
+    assert.equal(expected, result);
+});
+
+test('Fizz test - number divisible by 3', function(assert) {
+    const inputNumber = 9;
+    const expected = 'Fizz';
     const result = fizzy(inputNumber);
     assert.equal(expected, result);
 });
