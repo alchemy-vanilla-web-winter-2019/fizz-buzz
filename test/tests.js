78abeff0871e7cc8539c191a0e8254033cf70ae0
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-01-29 16:14:54 -0800

    added Buzz test and passed

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,9 @@
 const test = QUnit.test;
 
 function fizzBuzz(input) {
-    if(input % 3 === 0) {
+    if(input % 5 === 0) {
+        return 'Buzz';
+    } else if(input % 3 === 0) {
         return 'Fizz';
     };
 }
@@ -10,7 +12,12 @@ test('test if input number is divisible by 3', function(assert) {
     const inputNumber = 3;
     const result = fizzBuzz(inputNumber);
     const expected = 'Fizz';
-
     assert.equal(result, expected);
 });
 
+test('test if input number is divisible by 5', function(assert) {
+    const inputNumber = 5;
+    const result = fizzBuzz(inputNumber);
+    const expected = 'Buzz';
+    assert.equal(result, expected);
+});
