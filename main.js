9361bdbf73b882730ee85011255fa6afcd06498d
Ben Parisot <me@benparisot.com>
2019-01-29 16:27:46 -0800

    get results to display on html page

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ function fizzBuzz() {
     let integerInput = document.getElementById('number').value;
     console.log(integerInput);
 
+    document.getElementById('result').innerHTML = 'Your number is: ' + integerInput;
+
 }
