b30b08f514bf66666ebc27186ec9bfee7be7ba97
Heather <unphurlng@gmail.com>
2019-01-29 17:02:53 -0800

    solo build end

---
+++ test/tests.js
@@ -0,0 +1,17 @@
+const test = QUnit.test;
+
+// kept trying different things ... could not remember
+// enough to even get through the first test.
+
+function fizzBuzz(num) {
+
+}
+
+
+
+test('return num', function(assert) {
+    let num = 1;
+    let result = fizzBuzz(num);
+    assert.equal(result, num);
+});
+
