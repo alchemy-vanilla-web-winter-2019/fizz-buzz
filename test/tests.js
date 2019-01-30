b332dd8f4094644f09da61019973d521f848a8fc
Parker Helland <hellandp@gmail.com>
2019-01-29 17:00:01 -0800

    Fizz returns when enter 3 with passing test

--- test/tests.js
+++ test/tests.js
@@ -3,12 +3,21 @@ const test = QUnit.test;
 function fizzBuzz(number) {
     if(number % 15 === 0) {
         return 'FizzBuzz';
+    } else if(number % 3 === 0) {
+        return 'Fizz';
     }
 }
 
 test('if number is divisible by 3, 5 or 15 then return FizzBuzz', function(assert) {
     const inputNumber = 15;
     const expected = 'FizzBuzz';
+    const result = fizzBuzz(inputNumber);
+    assert.equal(result, expected);
+});
+
+test('if number is divisible by 3 then return Fizz', function(assert){
+    const inputNumber = 3;
+    const expected = 'Fizz';
     const result = fizzBuzz(inputNumber);
     assert.equal(result, expected);
 });
