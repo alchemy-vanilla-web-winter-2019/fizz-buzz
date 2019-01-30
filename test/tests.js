4018d2fb82f39e1eed22dd384ed13148b6a1b185
mcnadams <mcneilbo@oregonstate.edu>
2019-01-29 16:17:48 -0800

    added test for divisible by 3 and 5 returns FizzBuzz

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,21 @@
 const test = QUnit.test;
 
 function fizzBuzz(number) {
-    if(number % 3 === 0) {
+    if(number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+    }
+    else if(number % 3 === 0) {
         return 'Fizz';
     }
     else if(number % 5 === 0) {
         return 'Buzz';
     }
+    else {
+        return number;
+    }
 }
+
+test('return string 1 2 Fizz 4 for input 4', function(assert))
 
 test('replace numbers divisible by 3 with Fizz', function(assert) {
     let num = 3;
@@ -19,4 +27,16 @@ test('replace numbers divisible by 5 with Buzz', function(assert) {
     let num = 5;
     let result = fizzBuzz(num);
     assert.equal(result, 'Buzz');
+});
+
+test('replace numbers divisible by 3 and 5 with FizzBuzz', function(assert) {
+    let num = 15;
+    let result = fizzBuzz(num);
+    assert.equal(result, 'FizzBuzz');
+});
+
+test('numbers not divisible by 3 or 5 return the number', function(assert) {
+    let num = 7;
+    let result = fizzBuzz(num);
+    assert.equal(result, num);
 });
