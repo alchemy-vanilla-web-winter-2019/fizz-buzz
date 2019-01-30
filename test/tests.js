24b74fc5e7f21eba30c896249e7471f4a1bd7a49
mcnadams <mcneilbo@oregonstate.edu>
2019-01-29 16:12:31 -0800

    added test for divisible by 5 returns buzz

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,22 @@
 const test = QUnit.test;
 
 function fizzBuzz(number) {
-    if(number === 3){
-        return 'fizz';
+    if(number % 3 === 0) {
+        return 'Fizz';
+    }
+    else if(number % 5 === 0) {
+        return 'Buzz';
     }
 }
 
-test('replace numbers divisible by 3 with fizz', function(assert) {
+test('replace numbers divisible by 3 with Fizz', function(assert) {
     let num = 3;
     let result = fizzBuzz(num);
-    assert.equal(result, 'fizz');
+    assert.equal(result, 'Fizz');
+});
+
+test('replace numbers divisible by 5 with Buzz', function(assert) {
+    let num = 5;
+    let result = fizzBuzz(num);
+    assert.equal(result, 'Buzz');
 });
