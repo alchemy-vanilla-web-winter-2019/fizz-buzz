dc433cbb378b0310692294976a24c8ce2148327a
Ben Parisot <me@benparisot.com>
2019-01-29 16:41:54 -0800

    push resulst to new array

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 function fizzBuzz() {
+    let newArray = [];
+
+
     let integer = document.getElementById('number').value;
     let result = '';
     const wordOne = 'fizz';
     const wordTwo = 'buzz';
 
-    if(integer % 15 === 0) {
-        result = wordOne + wordTwo;
+    for(i = 1; i <= integer; i++) {
+        if(i % 15 === 0) {
+            result = wordOne + wordTwo;
+        }
+        else if(i % 5 === 0) {
+            result = wordTwo;
+        }
+        else if(i % 3 === 0) {
+            result = wordOne;
+        }
+        else {
+            result = i;
+        }
+        newArray.push(' ' + result);
     }
-    else if(integer % 5 === 0) {
-        result = wordTwo;
-    }
-    else if(integer % 3 === 0) {
-        result = wordOne;
-    }
-    else {
-        result = integer;
-    }
+    console.log(newArray);
 
-    document.getElementById('result').innerHTML = 'Your number is: ' + result;
+    document.getElementById('result').innerHTML = newArray.toString();
 }
