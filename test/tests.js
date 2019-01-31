d2120b64d1e87e87d9cc8f1d17ce269a59b0d862
Heather <unphurlng@gmail.com>
2019-01-31 05:39:33 -0800

    test red/green for buzz

--- test/tests.js
+++ test/tests.js
@@ -4,6 +4,8 @@ const test = QUnit.test;
 function fizzBuzz(num) {
     if(num % 3 === 0) {
         return 'Fizz';
+    } else if(num % 5 === 0) {
+        return 'Buzz';
     }
 }
 
@@ -13,5 +15,12 @@ test('return Fizz if num is divisible by 3', function(assert) {
     const num = 12;
     const result = fizzBuzz(num);
     const expected = 'Fizz';
+    assert.equal(result, expected);
+});
+
+test('return Buzz if num is divisible by 5', function(assert) {
+    const num = 10;
+    const result = fizzBuzz(num);
+    const expected = 'Buzz';
     assert.equal(result, expected);
 });
