b60abf308fdeafcc5f18fda808ff7cc862172ed6
Heather <unphurlng@gmail.com>
2019-01-31 05:45:06 -0800

    tested multiple nums for 3, 5, 15 - all red/green working as expected

--- test/tests.js
+++ test/tests.js
@@ -2,14 +2,14 @@ const test = QUnit.test;
 
 
 function fizzBuzz(num) {
-    if(num % 3 === 0) {
+    if(num % 15 === 0) {
+        return 'FizzBuzz';
+    } else if (num % 3 === 0) {
         return 'Fizz';
     } else if(num % 5 === 0) {
         return 'Buzz';
     }
 }
-
-
 
 test('return Fizz if num is divisible by 3', function(assert) {
     const num = 12;
@@ -22,5 +22,12 @@ test('return Buzz if num is divisible by 5', function(assert) {
     const num = 10;
     const result = fizzBuzz(num);
     const expected = 'Buzz';
+    assert.equal(result, expected);
+});
+
+test('return FizzBuzz if num is divisible by 15', function(assert) {
+    const num = 15;
+    const result = fizzBuzz(num);
+    const expected = 'FizzBuzz';
     assert.equal(result, expected);
 });
