3b27789fbb8b87cceba811666f5348bfc40e1b25
Liz <jesusridesthebus.com>
2019-02-08 15:18:08 -0800

    fixed problems so tests pass

--- tests.js
+++ tests.js
@@ -1,30 +1,30 @@
-const test = Qunit.test;
+const test = QUnit.test;
 
-function FizzBuzz(input){
+function fizzBuzz(input) {
     if(input % 15 === 0) {
-    return 'FizzBuzz';
+        return 'FizzBuzz';
     } else if(input % 5 === 0) {
-    return 'Buzz';
+        return 'Buzz';
     } else if(input % 3 === 0) {
-    return 'Fizz';
+        return 'Fizz';
     } else
-    return (input);
-    }
+        return (input);
 }
 
-
-test('input divisible by 15', function(assert){
+test('input divisible by 15', function(assert) {
     const input = 15;
-    const result = FizzBuzz(input);
-    assert.equal = (result, 'FizzBuzz');
+    const result = fizzBuzz(input);
+    assert.equal(result, 'FizzBuzz');
 });
-test('input divisible by 5', function(assert){
+
+test('input divisible by 5', function(assert) {
     const input = 5;
-    const result = Buzz(input);
-    assert.equal = (result, 'Buzz');
+    const result = fizzBuzz(input);
+    assert.equal(result, 'Buzz');
 });
-test('input divisible by 3', function(assert){
+
+test('input divisible by 3', function(assert) {
     const input = 3;
-    const result = Fizz(input);
-    assert.equal = (result, 'Fizz');
+    const result = fizzBuzz(input);
+    assert.equal(result, 'Fizz');
 });
